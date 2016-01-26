b11cbcb34408726b1304de78520e0e4be6659f69
nh <nhelfenberger@vev.fr>
2016-01-26 16:11:05 +0100

    changeuserpermitnavbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,26 +3,26 @@ module ApplicationHelper
 private
   def select_compagny_user
     #In NavBar Select a good compagny context
-    if current_user
-      @choisecompagny = User.find(current_user.id).compagnies
-      if
-        params[:compagny_id] == nil
-        params[:compagny_id] = Userpermit.where(:user => current_user, :priority => true).first
-         if
-          params[:compagny_id] == nil
-          params[:compagny_id] = Userpermit.where(:user => current_user).first.compagny_id
+      if current_user
+        if  User.find(current_user.id).compagnies.count == 0
+          @choisecompagny = nil
+        else
           @choisecompagny = User.find(current_user.id).compagnies
-          else
-        params[:compagny_id] = Userpermit.where(:user => current_user, :priority => true).first.compagny_id
-        @choisecompagny = User.find(current_user.id).compagnies
+          if
+            params[:compagny_id] == nil
+            params[:compagny_id] = Userpermit.where(:user => current_user, :priority => true).first
+             if
+              params[:compagny_id] == nil
+              params[:compagny_id] = Userpermit.where(:user => current_user).first.compagny_id
+              @choisecompagny = User.find(current_user.id).compagnies
+              else
+            params[:compagny_id] = Userpermit.where(:user => current_user, :priority => true).first.compagny_id
+            @choisecompagny = User.find(current_user.id).compagnies
+              end
           end
-      end
-      end
+          end
     end
-
-
-
-
+  end
 
 
 
